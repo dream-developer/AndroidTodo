4be619dcd63cb72f89fd066ca0e820f868afa784
git-edifice <git_edifice@yahoo.co.jp>
2024-05-03 15:19:23 +0900

    データーベースの定義

--- app/src/main/java/com/example/androidtodo/MainActivity.kt
+++ app/src/main/java/com/example/androidtodo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidtodo
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,7 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.Update
 import com.example.androidtodo.ui.theme.AndroidTodoTheme
+import kotlinx.coroutines.flow.Flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +41,56 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Entity(tableName = "todos") // 1
+data class Todo(
+    @PrimaryKey(autoGenerate = true) // 2
+    val id: Int = 0,
+    val content: String = "", // 3
+    val created_at: Long = 0, // 4
+)
+
+@Dao // 1
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // 2
+    suspend fun create(todos: Todo)
+
+    @Query("SELECT * FROM todos ORDER BY created_at DESC")
+    fun getAll(): Flow<List<Todo>> // 3
+
+    @Query("SELECT * FROM todos WHERE id = :id")
+    fun getById(id: Int): Flow<Todo>?
+
+    @Update
+    suspend fun update(todo: Todo)
+
+    @Delete
+    suspend fun delete(todo: Todo)
+}
+
+@Database(entities = [Todo::class], version = 1, exportSchema = false) // 1
+
+abstract class AppDatabase : RoomDatabase() { // 2
+    abstract fun todoDao(): TodoDao
+}
+
+class RoomApplication : Application() { // 3
+
+    companion object { // 4
+        lateinit var database: AppDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        database = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "todos" // 5
+        ).build()
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
